acd844a884e6f636788ee768e8b3b04fc333aed7
Ujjwal Goel <ugoel13@gmail.com>
2019-09-26 22:45:20 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,2 @@
-require("./login.es6");
 document.write("here is my app jfwwefwefew fwefwfewfew  s file in src folder");
-console.log("Ujjwal Goel here!!!!!!!!!!!!!");
+console.log("hello this is console");
